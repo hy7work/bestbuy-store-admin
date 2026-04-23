ef38c7684890831312717999b7475f9fbd64185b
hycst <yang0371@algonquinlive.com>
2026-04-23 03:35:43 -0400

    CI/CD demo change

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="header">
-      <h2>Manage Products demo for CI/CD</h2>
+      <h2>Manage Products project demo for CI/CD</h2>
       <router-link to="/product/add">
         <button class="add-btn">+ Add Product</button>
       </router-link>
